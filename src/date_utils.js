5996ef2c8ab9dd6b319924675060fdf0d04a75a3
tejalkukadiya <tejalkukadiya@gmail.com>
2020-07-24 11:45:26 +0530

    changed for select value

--- src/date_utils.js
+++ src/date_utils.js
@@ -162,7 +162,7 @@ export function safeDateFormat(date, { dateFormat, locale }) {
     (date &&
       formatDate(
         date,
-        Array.isArray(dateFormat) ? dateFormat[0].toLowerCase() : dateFormat.toLowerCase(),
+       'MM/dd/yyyy',
         (locale: locale)
       )) ||
     ""
