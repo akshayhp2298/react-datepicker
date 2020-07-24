2bf96b66eec83d12220114ffd087c4bc6bd15ca5
tejalkukadiya <tejalkukadiya@gmail.com>
2020-07-24 18:19:58 +0530

    change for parse date

--- src/date_utils.js
+++ src/date_utils.js
@@ -111,7 +111,8 @@ export function parseDate(value, dateFormat, locale, strictParsing) {
       .join("");
 
     if (value.length > 0) {
-      parsedDate = parse(value, dateFormat.slice(0, value.length).replace('DD','dd').replace('YYYY','yyyy'), new Date());
+      console.log(dateFormat, value , value.length, 'test');
+      parsedDate = parse(value, dateFormat.replace('DD','dd').replace('YYYY','yyyy'), new Date());
     }
 
     if (!isValid(parsedDate)) {
